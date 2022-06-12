dbb1bb041355185873467bbf148e6f067cbba62c
yakirteng <yakirteng@tencent.com>
2022-06-12 16:41:09 +0800

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by yakir on 2022/6/12.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
